37cfb0ebd3ce6d796edcf09fe43304ddee64a773
wuxq <wuxiaoqian88@126.com>
2013-11-12 11:13:47 +0800

    Update index.js
    签名验证的问题，验证代码错误

--- routes/index.js
+++ routes/index.js
@@ -96,7 +96,7 @@ var getMySign = function (params) {
     var sPara=[];//转换为数组利于排序 除去空值和签名参数
     if(!params) return null;
     for(var key in params) {
-        if((!params[key])|| params[key] == "sign" || params[key] == "sign_type"){
+        if((!params[key])|| key == "sign" || key == "sign_type"){
             console.log('null:'+key);
             continue;
         } ;
@@ -119,7 +119,7 @@ var getMySign = function (params) {
     //body=Hello&buyer_email=13758698870&buyer_id=2088002007013600&discount=-5&extra_common_param=你好，这是测试商户的广告。&gmt_close=2008-10-22 20:49:46&gmt_create=2008-10-22 20:49:31&gmt_payment=2008-10-22 20:49:50&gmt_refund=2008-10-29 19:38:25&is_total_fee_adjust=N&notify_id=70fec0c2730b27528665af4517c27b95&notify_time=2009-08-12 11:08:32&notify_type=交易状态同步通知(trade_status_sync)&out_trade_no=3618810634349901&payment_type=1&price=10.00&quantity=1&refund_status=REFUND_SUCCESS&seller_email=chao.chenc1@alipay.com&seller_id=2088002007018916&sign=_p_w_l_h_j0b_gd_aejia7n_ko4_m%2Fu_w_jd3_nx_s_k_mxus9_hoxg_y_r_lunli_pmma29_t_q%3D%3D&sign_type=DSA&subject=iphone手机&total_fee=10.00&trade_no=2008102203208746&trade_status=TRADE_FINISHED&use_coupon=N
 
     var crypto = require('crypto');
-    return crypto.createHash('md5').update(prestr).digest("hex");
+    return crypto.createHash('md5').update(prestr, AlipayConfig.input_charset).digest("hex");
 };
 
 var requestUrl=function(host,path,callback){
